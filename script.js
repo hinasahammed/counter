2877339ffc6b7f77ce62927402a3b3d401d2e920
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2021-10-05 11:35:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let count = 0;
+function plus(){
+    let val=document.getElementById("num").innerHTML;
+  let num=Number(val)+1
+  document.getElementById("num").innerHTML=num;
+}
+
+function minus(){
+    let val=document.getElementById("num").innerHTML;
+  let num=Number(val)-1
+  document.getElementById("num").innerHTML=num;
+}
